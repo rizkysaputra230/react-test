38c5ec7e2ab447a5fae22aa5d688d9401078aef4
faisal <faisal@hellonimbly.com>
2021-01-12 17:34:58 +0700

    add comment for learning more

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+  /**
+   * Try inject props into navbar
+   * 
+   * Add Routes & also make the component (Just Dummy First (Title on the blank page)) for
+   *  - Users
+   *  - Jobs
+   *  - Sallary  
+   *  - Login
+   *  - Register
+   */
 function App() {
   return (
     <Router>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
+const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
 
-const COLOR = ['primary', 'blue', 'red', 'green'];
+const COLOR = ["primary", "blue", "red", "green"];
 
+/**
+ * Find out what is it, functional component & class component
+ */
+
+ 
+/**
+   * Find out what destructuring in javascript. 
+   */
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
   buttonSize,
-  buttonColor
+  buttonColor,
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
+  /**
+   * Find out what is it ternary operator 
+   */
   const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
   return (
@@ -27,6 +40,9 @@ export const Button = ({
       onClick={onClick}
       type={type}
     >
+      {/**
+       * Find out what is it children props
+       */}
       {children}
     </button>
   );
